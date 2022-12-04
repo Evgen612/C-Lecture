191b133d27d51068cfcedad762fffa671ef64fea
evg_smi <evg_smi@mail.ru>
2022-12-04 13:39:49 +0300

    methodarray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1, 9, 12, 45, 88, 102, 45, 12, 54};
+
+int n = array.Length;
+int find = 45;
+
+int index = 0;
+
+while (index < n)
+{
+        if(array[index] == find)
+        {
+            Console.WriteLine(index);
+        } 
+    index++;
+}
+
